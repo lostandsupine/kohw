02bf6658ec6f4765ffdfb1c87d10a8f04475f993
aarn <aaronmaus@gmail.com>
2017-03-04 15:38:28 -0800

    adding initial unity project

---
+++ Assets/scripts/input_manager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class input_manager : MonoBehaviour {
+
+	public static bool is_paused = false;
+	private bool pause_down = false;
+	private float velocity = 7f;
+	private Vector2 current_direction = new Vector2(0f,0f);
+	private Vector2 last_direction = new Vector2(0f,-1f);
+
+	void Start () {
+
+	}
+
+	public Vector2 get_direction(){
+		return(last_direction);
+	}
+		
+	void Update () {
+		if((Input.GetKey(KeyCode.Escape)) && !pause_down)
+		{
+			Time.timeScale = 1.0f - Time.timeScale;
+			is_paused = !is_paused;
+			pause_down = true;
+		}
+		float move_x = 0;
+		float move_y = 0;
+		pause_down = Input.GetKey (KeyCode.Escape);
+
+		if (!is_paused) {
+			
+			if (!is_paused && (Input.GetKey (KeyCode.S))) {
+				//GameObject.Find ("leila").GetComponent<leila_walk> ().move_leila (0,velocity);
+				move_y--;
+			}
+			if (!is_paused  && (Input.GetKey (KeyCode.A))) {
+				//GameObject.Find ("leila").GetComponent<leila_walk> ().move_leila (1,velocity);
+				move_x--;
+			}
+			if (!is_paused && (Input.GetKey (KeyCode.W))) {
+				//GameObject.Find ("leila").GetComponent<leila_walk> ().move_leila (2,velocity);
+				move_y++;
+			}
+			if (!is_paused && (Input.GetKey (KeyCode.D))) {
+				//GameObject.Find ("leila").GetComponent<leila_walk> ().move_leila (3,velocity);
+				move_x++;
+			}
+			if (move_x != 0 && move_y == 0) {
+				GameObject.Find ("player").GetComponent<player_controller> ().move_player (2 + (int)move_x, velocity);
+				GameObject.Find ("player").GetComponent<player_controller> ().moving = 1;
+				Debug.Log("moving input");
+			} else if (move_x == 0 && move_y != 0) {
+				GameObject.Find ("player").GetComponent<player_controller> ().move_player (1 + (int)move_y, velocity);
+				GameObject.Find ("player").GetComponent<player_controller> ().moving = 1;
+				Debug.Log("moving input");
+			} else if (move_x != 0 && move_y != 0) {
+				GameObject.Find ("player").GetComponent<player_controller> ().move_player (2 + (int)move_x, velocity / 1.414214f);
+				GameObject.Find ("player").GetComponent<player_controller> ().move_player (1 + (int)move_y, velocity / 1.414214f);
+				GameObject.Find ("player").GetComponent<player_controller> ().moving = 1;
+				Debug.Log("moving input");
+			} else {
+				GameObject.Find ("player").GetComponent<player_controller> ().moving = 0;
+			}
+				
+			if (Input.GetMouseButtonDown (0)) {
+				Debug.Log ("mouse button down");
+				GameObject.Find ("player").GetComponent<player_controller> ().do_attack ();
+			}
+
+
+			current_direction = new Vector2 (move_x, move_y);
+			if (current_direction.magnitude > 0) {
+				last_direction = current_direction;
+			}
+
+
+		}
+	}
+}
---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_controller : MonoBehaviour {
+
+	public Sprite[] sprite_list;
+	private int direction = 0;
+	public int moving = 0;
+	private int current_frame = 0;
+	private float angle;
+	private Vector3 mouse_position;
+	private Vector3 player_position;
+	private Vector3 weapon_position;
+	private bool attacking = false;
+	private float attack_start_time;
+	private float attack_time_length = 0.25f;
+	private bool recovering = false;
+	private float recover_start_time;
+	private float recover_time_length = 0.25f;
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "enemy"){
+			//GameObject.Find ("game_over_text").GetComponent<game_over_script> ().game_over ();
+		}
+	}
+
+	public void do_attack(){
+		Debug.Log ("do attack");
+		if (!attacking && !recovering) {
+			attack_start_time = Time.time;
+		}
+		attacking = true;
+	}
+
+	public void do_recover(){
+		Debug.Log ("do recover");
+		if (!recovering && !attacking) {
+			recover_start_time = Time.time;
+		}
+		recovering = true;
+	}
+
+	private void set_weapon_rotation(){
+		mouse_position = Input.mousePosition;
+		weapon_position = Camera.main.WorldToScreenPoint (this.transform.GetChild(1).transform.GetChild(0).transform.position);
+		angle = Mathf.Atan2 (mouse_position.y - weapon_position.y, mouse_position.x - weapon_position.x) * Mathf.Rad2Deg;
+		this.transform.GetChild(1).transform.GetChild(0).transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle-90));
+	}
+
+	private void set_rotation(){
+		mouse_position = Input.mousePosition;
+		player_position = Camera.main.WorldToScreenPoint (this.transform.position);
+		angle = Mathf.Atan2 (mouse_position.y - player_position.y, mouse_position.x - player_position.x) * Mathf.Rad2Deg;
+		this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle-90));
+	}
+
+	public void move_player(int in_direction, float velocity_in){
+		direction = in_direction;
+		//moving = 1;
+		switch (in_direction) {
+		case 0:
+			this.transform.Translate ((Vector3.down * velocity_in) * Time.deltaTime,Space.World);
+			break;
+		case 1:
+			this.transform.Translate ((Vector3.left * velocity_in) * Time.deltaTime,Space.World);
+			break;
+		case 2:
+			this.transform.Translate ((Vector3.up * velocity_in) * Time.deltaTime,Space.World);
+			break;
+		case 3:
+			this.transform.Translate ((Vector3.right * velocity_in) * Time.deltaTime,Space.World);
+			break;
+		}
+	}
+
+	public void translate_player(int in_direction, float velocity_in){
+		direction = in_direction;
+		switch (in_direction) {
+		case 0:
+			this.transform.Translate ((Vector3.down * velocity_in));
+			break;
+		case 1:
+			this.transform.Translate ((Vector3.left * velocity_in));
+			break;
+		case 2:
+			this.transform.Translate ((Vector3.up * velocity_in));
+			break;
+		case 3:
+			this.transform.Translate ((Vector3.right * velocity_in));
+			break;
+		}
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		/*if (1 == moving) {
+			current_frame = (int)((15 * Time.fixedTime) % 12);
+			GetComponent<SpriteRenderer> ().sprite = sprite_list [current_frame];
+		} else {
+			current_frame = 0;
+			GetComponent<SpriteRenderer> ().sprite = sprite_list [current_frame];
+		}*/
+		set_rotation ();
+		//set_weapon_rotation ();
+		//this.transform.GetChild (1).transform.localEulerAngles = arm_rotation + new Vector3 (0, 0, 1);
+		//arm_rotation = this.transform.GetChild (1).transform.localEulerAngles;
+		if (attacking) {
+			if (this.transform.GetChild (1).transform.localRotation.eulerAngles.z < 90f) {
+				this.transform.GetChild (1).transform.RotateAround (this.transform.position, Vector3.forward, 500 * Time.deltaTime);
+			} 
+			if (this.transform.GetChild (1).transform.localRotation.eulerAngles.z > 90f) {
+				this.transform.GetChild (1).transform.RotateAround (this.transform.position, Vector3.forward, 90-this.transform.GetChild (1).transform.localRotation.eulerAngles.z);
+			} 
+			if ((Time.time - attack_start_time) >= attack_time_length) {
+				attacking = false;
+				do_recover ();
+			}
+		}
+		if (recovering) {
+			if (this.transform.GetChild (1).transform.localRotation.eulerAngles.z < 100f) {
+				this.transform.GetChild (1).transform.RotateAround (this.transform.position, Vector3.forward, -500 * Time.deltaTime);
+			}
+			if (this.transform.GetChild (1).transform.localRotation.eulerAngles.z > 180f) {
+				this.transform.GetChild (1).transform.RotateAround (this.transform.position, Vector3.forward, -this.transform.GetChild (1).transform.localRotation.eulerAngles.z);
+			} 
+			if ((Time.time - recover_start_time) >= recover_time_length) {
+				recovering = false;
+			}
+		}
+		if (!attacking && !recovering) {
+			this.transform.GetChild (1).transform.RotateAround (this.transform.position, Vector3.forward, -this.transform.GetChild (1).transform.localRotation.eulerAngles.z);
+		}
+	}
+}
